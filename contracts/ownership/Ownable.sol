166b7ae417b5b540e53345c1e6ecd33e260c970b
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2023-04-17 15:50:54 +0000

    completed ownable contract

---
+++ contracts/ownership/Ownable.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+contract Ownable is Context {
+    address private _owner;
+
+    constructor(){
+        _owner = _msgSender();
+    }
+
+    modifier onlyOwner() {
+        require(_msgSender() == _owner);
+        _;
+    }
+
+    function getOwner() public view returns(address){
+        return _owner;
+    }
+
+    function transferOwnership(address account) internal onlyOwner{
+        address prevOwner = _owner;
+        _owner = account;
+        emit OwnershipTransfered(prevOwner, account);
+    }
+
+    function renounceOwnership() internal onlyOwner{
+        transferOwnership(address(0));
+    }
+
+    event OwnershipTransfered(address indexed from, address indexed to);
+}
